3d3f6fc3d94d6dffb34d1e6f3e2bcb8529c6e3db
zhouyou <kaka252@sina.com>
2016-12-14 17:08:53 +0800

    定制信息接收者

--- remote/src/main/java/com/zhouyou/remote/client/Receiver.java
+++ remote/src/main/java/com/zhouyou/remote/client/Receiver.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.os.RemoteException;
+import android.util.Log;
 
 import com.zhouyou.remote.IMusicReceiver;
 import com.zhouyou.remote.Music;
+import com.zhouyou.remote.State;
 
 /**
  * 作者：ZhouYou
@@ -22,14 +24,83 @@ public class Receiver extends IMusicReceiver.Stub {
 //        b.putInt("state", currState);
 //        msg.setData(b);
 //        handler.sendMessage(msg);
-
-
-
+        dispatch(currState);
     }
+
+    /**
+     * 消息分发
+     * @param currState
+     */
+    private void dispatch(int currState) {
+        switch (currState) {
+            case State.IDLE: // 闲置
+                break;
+            case State.INITIALIZED: // 初始化
+                break;
+            case State.PREPARING: // 正在准备
+                break;
+            case State.PREPARED: // 准备就绪
+                break;
+            case State.PAUSED: // 暂停
+                handler.sendEmptyMessage(ACTION_PROGRESS_SUSPEND);
+                handler.sendEmptyMessage(ACTION_NOTIFICATION);
+            case State.COMPLETED: // 播放完成
+//                handler.sendEmptyMessageDelayed(isPlayBack ? ACTION_PLAY_BACK : ACTION_PLAY_NEXT, 100);
+                handler.sendEmptyMessage(ACTION_INIT);
+                break;
+            case State.STOPPED: // 播放终断
+                handler.sendEmptyMessage(ACTION_INIT);
+                break;
+            case State.END: // 结束
+                handler.sendEmptyMessage(ACTION_INIT);
+                break;
+            case State.ERROR: // 错误
+//                Toast.ss("音频文件出错");
+                handler.sendEmptyMessage(ACTION_INIT);
+                handler.sendEmptyMessageDelayed(ACTION_PLAY_NEXT, 2000);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private static final int ACTION_INIT = 0;
+    private static final int ACTION_PLAY_NEXT = 1; // 播放下一首
+    private static final int ACTION_PLAY_BACK = 2; // 播放上一首
+    private static final int ACTION_PROGRESS_UPDATE = 3; // 更新播放时间
+    private static final int ACTION_PROGRESS_SUSPEND = 4; // 暂停
+    private static final int ACTION_NOTIFICATION = 5; // 发送通知栏消息
 
     private Handler handler = new Handler(new Handler.Callback() {
         @Override
-        public boolean handleMessage(Message message) {
+        public boolean handleMessage(Message msg) {
+            switch (msg.what) {
+                case ACTION_INIT:
+//                    currentPosition = 0;
+                    break;
+                case ACTION_PLAY_NEXT:
+//                    playNext();
+                    break;
+                case ACTION_PLAY_BACK:
+//                    playBack();
+                    break;
+                case ACTION_PROGRESS_UPDATE:
+//                    if (!isProgressControlledByUser) {
+//                        AudioManagerFactory.get().createProgressPublisher().notifySubscribers(getCurrentAudioProgress(), getCurrentAudioDuration());
+//                        handler.sendEmptyMessageDelayed(ACTION_PROGRESS_UPDATE, 1000);
+//                    } else {
+//                        handler.sendEmptyMessage(ACTION_PROGRESS_UPDATE);
+//                    }
+                    break;
+                case ACTION_PROGRESS_SUSPEND:
+                    handler.removeMessages(ACTION_PROGRESS_UPDATE);
+                    break;
+                case ACTION_NOTIFICATION:
+//                    NotificationReceiver.get().sendNotification();
+                    break;
+                default:
+                    break;
+            }
             return true;
         }
     });
